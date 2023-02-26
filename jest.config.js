2de674ea5af48d06ccdcc42851c0bec5721d47cb
ChenNima <fennu637@sina.com>
2023-02-26 16:50:31 +0800

    add ci (#1)

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
-jest.config.jsrequire('ts-node/register');
-
 module.exports = {
-  'moduleFileExtensions': [
+  moduleFileExtensions: [
     'js',
     'json',
     'ts',
   ],
-  'rootDir': 'lib',
-  'testRegex': '/lib/.*\\.spec\\.(ts|js)$',
-  'preset': 'ts-jest',
+  coverageThreshold: {
+    global: {
+        lines: 95,
+    },
+},
+  rootDir: 'lib',
+  testRegex: '/lib/.*\\.spec\\.(ts|js)$',
+  preset: 'ts-jest',
 };
